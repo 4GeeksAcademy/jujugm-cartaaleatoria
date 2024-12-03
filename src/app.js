cd628985dd9665172b30feec80f750ff8071df9b
Jujugm <julia7backup@gmail.com>
2024-12-03 07:25:22 +0000

    random-card-finalizada

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function generatecard() {
+  let Cardicon = ["♥️", "♦️", "♠️", "♣️"];
+  let Value = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomCardicon = Math.floor(Math.random() * 4);
+  let randomValue = Math.floor(Math.random() * 13);
+
+  document.querySelector(".card-icon-left").innerHTML =
+    Cardicon[randomCardicon];
+  document.querySelector(".card-icon-right").innerHTML =
+    Cardicon[randomCardicon];
+  document.querySelector(".cardText h1").innerHTML = Value[randomValue];
+
+  let color = randomCardicon === 0 || randomCardicon === 1 ? "red" : "black";
+  document.querySelector(".card").style.color = color;
+
+  let refresh = document.getElementById("reload");
+  reload.addEventListener("click", _ => {
+    location.reload();
+  });
+
+  console.log("Palo:", Cardicon[randomCardicon]);
+  console.log("Valor:", Value[randomValue]);
 };
